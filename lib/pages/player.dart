39253d07eb194c5a3707bda8da17fbe63e3a50a4
Fabian Zürker Aguilar <fzuerker@gmail.com>
2023-01-07 19:43:58 +0100

    Add cosinuss ble class

--- lib/pages/player.dart
+++ lib/pages/player.dart
@@ -3,6 +3,7 @@ import "package:flutter/material.dart";
 import 'package:just_audio/just_audio.dart';
 import "package:on_audio_query/on_audio_query.dart";
 import 'package:google_fonts/google_fonts.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 
 
 class Player extends StatefulWidget {
---
+++ lib/utils/ble-gyroscope.dart
@@ -0,0 +1,100 @@
+import 'dart:typed_data';
+
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
+class BLEGyroscope{
+  bool _isConnected = false;
+  final flutterReactiveBle = FlutterReactiveBle();
+  final _gyroscopeService = Uuid.parse("0000a000-1212-efde-1523-785feabcd123");
+  final _gyroscopeCharacteristic = Uuid.parse("0000a001-1212-efde-1523-785feabcd123");
+  final _gyroscopeConf = [0x32, 0x31, 0x39, 0x32, 0x37, 0x34, 0x31, 0x30, 0x35, 0x39, 0x35, 0x35, 0x30, 0x32, 0x34, 0x35];
+  DiscoveredDevice? _device;
+  int _accX = 0;
+  int _accY = 0;
+  int _accZ = 0;
+
+  void _confGyro() async{
+    if(_isConnected){
+      final characteristic = QualifiedCharacteristic(
+        serviceId: _gyroscopeService,
+        characteristicId: _gyroscopeCharacteristic,
+        deviceId: _device!.id,
+      );
+      await flutterReactiveBle.writeCharacteristicWithResponse(characteristic, value: _gyroscopeConf);
+    }
+  }
+
+  void _updateAcc(rawData){
+    Int8List bytes = Int8List.fromList(rawData);
+    _accX = bytes[14];
+    _accY = bytes[16];
+    _accZ = bytes[18];
+  }
+
+  Stream<int> get accXStream => flutterReactiveBle.subscribeToCharacteristic(
+    QualifiedCharacteristic(
+      serviceId: _gyroscopeService,
+      characteristicId: _gyroscopeCharacteristic,
+      deviceId: _device!.id,
+    ),
+  ).map((event) {
+    _updateAcc(event);
+    return _accX;
+  });
+
+  Stream<int> get accYStream => flutterReactiveBle.subscribeToCharacteristic(
+    QualifiedCharacteristic(
+      serviceId: _gyroscopeService,
+      characteristicId: _gyroscopeCharacteristic,
+      deviceId: _device!.id,
+    ),
+  ).map((event) {
+    _updateAcc(event);
+    return _accY;
+  });
+
+  Stream<int> get accZStream => flutterReactiveBle.subscribeToCharacteristic(
+    QualifiedCharacteristic(
+      serviceId: _gyroscopeService,
+      characteristicId: _gyroscopeCharacteristic,
+      deviceId: _device!.id,
+    ),
+  ).map((event) {
+    _updateAcc(event);
+    return _accZ;
+  });
+
+
+  void connect() async {
+    flutterReactiveBle.scanForDevices(withServices: [_gyroscopeService]).listen((scanResult) {
+      print('Found device: ${scanResult.id}');
+      if(!_isConnected){
+        flutterReactiveBle.connectToAdvertisingDevice(id: scanResult.id,
+            withServices: [_gyroscopeService],
+            prescanDuration: const Duration(seconds: 1)).listen((connectionState) {
+          print('Connection state: $connectionState');
+          if(connectionState == DeviceConnectionState.connected){
+            _isConnected = true;
+            _device = scanResult;
+            _confGyro();
+            flutterReactiveBle.subscribeToCharacteristic(
+                QualifiedCharacteristic(
+                  serviceId: _gyroscopeService,
+                  characteristicId: _gyroscopeCharacteristic,
+                  deviceId: _device!.id,
+                )
+            ).listen((data) {
+              _updateAcc(data);
+            });
+          }
+        });
+      }
+    });
+  }
+
+  get isConnected => _isConnected;
+  get accX => _accX;
+  get accY => _accY;
+  get accZ => _accZ;
+
+}
